73ce07cca59a1ce674ffafcc1e9fd6a54e6ed837
Josh Bain <joshuambain@gmail.com>
2024-07-17 16:45:43 -0400

    complete javascript functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity='roller-skate') {
+   return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work='go to the office') {
+   return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(visualFlair="*") {
+   return function innerWrapAdjective(adjective="special")  {
+      return `You are ${visualFlair}${adjective}${visualFlair}!`
+   }  
+   }
+
+   // const encouragingPromptFunction = wrapAdjective(!!!);
